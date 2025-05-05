4d519535a1a07319745e428a3d8955834e14517d
ReduceUI <115711792+ReduceUI@users.noreply.github.com>
2025-05-05 22:26:16 +1000

    GuiMain lab 5 optional

--- src/main/java/garden_planner/gui/GuiMain.java
+++ src/main/java/garden_planner/gui/GuiMain.java
@@ -6,8 +6,12 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.geometry.Insets;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+
 
 /**
  * NOTE: Do NOT run this class in IntelliJ.  Run 'RunGui' instead.
@@ -15,6 +19,8 @@ import javafx.stage.Stage;
 public class GuiMain extends Application {
 
     GardenPlanner planner;
+    TextField widthField;
+    TextField heightField;
 
     public GuiMain() {
         planner = new GardenPlanner();
@@ -41,14 +47,45 @@ public class GuiMain extends Application {
         }
 
         //creates a static rectangle to place on the pane
-        Rectangle rec = new Rectangle(100, 100);
-        pane.getChildren().add(rec);
-        rec.setX(600);
-        rec.setY(400);
+//        Rectangle rec = new Rectangle(100, 100);
+//        pane.getChildren().add(rec);
+//        rec.setX(600);
+//        rec.setY(400);
+
+//        widthField = new TextField();
+//        double width = planner.getBeds().get(0).getWidth();
+//        String str = Double.toString(width);
+//        widthField.setText(str);
+//        pane.getChildren().add(widthField);
+
+        // Display the width and height of the first bed
+        Pane leftPane = new VBox(10);
+        leftPane.setPadding(new Insets(5, 5, 5, 5));
+        leftPane.setStyle("-fx-background-color: #107700;");
+        root.setLeft(leftPane);
+
+        widthField = new TextField();
+        double width = planner.getBeds().get(0).getWidth();
+        String str = Double.toString(width);
+        widthField.setText(str);
+        leftPane.getChildren().add(widthField);
+
+        heightField = new TextField();
+        double height = planner.getBeds().get(0).getWidth();
+        str = Double.toString(height);
+        heightField.setText(str);
+        leftPane.getChildren().add(heightField);
+
+
+
+
+
+
+
 
         //creates the window and loads the root and children
         primaryStage.setTitle("Garden Planner");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
     }
 
